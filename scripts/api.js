dc8f6257450fa86ab1fc8d06deca43ce76de23bb
Robert Wiggins <wigginsro11@gmail.com>
2019-05-09 19:06:53 -0400

    implemented fetchHandler with comprehensive pseudocode

--- scripts/api.js
+++ scripts/api.js
@@ -4,19 +4,77 @@
 
 const api = (function() {
   
-  const BASE_URL = "https://thinkful-list-api.herokuapp.com/robertw/bookmarks"
+  // base URL for bookmarks server end point
+  const BASE_URL = "https://thinkful-list-api.herokuapp.com/robertw/bookmarks";
 
+  // returns a parsed json if fetch is valid, otherwise standardizes the handling of errors for all requests 
+  function fetchHandler(...params) {
+    // setup error in scope outside fetch promise chain to set up as undefined
+    // if it remains undefined in the function (error object statement is never hit)
+    // then the response was of a 200 level 
+    let error;
+    return fetch(...params)
+      .then(response => {
+        if (!response.ok) {
+          // if response is not 2xx, start building error object
+          // response.status collects the xxx error
+          error = { code: response.status };
 
-  function getBookmarks() {
-    
+          // if response is not JSON type (does not include any json text (char response)),
+          //  place Status Text in error object and immediately reject promise
+          // .statusText is a read-only property of the Response interface that
+          // contains the status message corresponding to the status code (i.e. OK for 200)
+          if (!response.headers.get('content-type').includes('json')) {
+            error.message = response.statusText;
+            // error is simply an object now that contains the response code and the OK vs FAIL, etc. case
+            // the promise.reject() function returns a Promise that is rejected. For debugging purposes 
+            // and selective error catching, it is useful to make reason an instanceof Error.
+            // we did not do that here, but included the earlier status codes collected. 
+            // The argument can be thought of as the communicated reason
+            return Promise.reject(error);
+          }
+        }
+
+        // otherwise, return parsed JSON. json() method is a Body method which takes the Response stream
+        // and reads it to completion. It returns a promise that resolves with the result of parsing the body
+        // text in JSON
+        return response.json();
+      })
+      .then(data => {
+      // if error has been defined i.e. if an error exists, place the JSON message into the error object and 
+      // reject the Promise with your error object so it lands in the next 
+      // catch. We should note how the API sends errors -- not all APIs
+      // will respond with a JSON object containing the message key
+        if (error) {
+          error.message = data.message;
+          return Promise.reject(error);
+        }
+
+        // otherwise, return the json as normal resolved Promise that now contains our data
+        return data;
+      });
   }
 
 
+
+  /** 
+   * Get all of the Bookmark data contained in the server, most of the logic of which is facilitated
+   * through the above fetchHandler() above. getBookmarks() returns a resolved Promise containing
+   * the data. Returned keys include: id, title, url, desc, rating 
+   */
+  function getBookmarks() {
+    // The get URL just requires the base url + /bookmarks, which is already done in the constant above
+    return fetchHandler(BASE_URL);
+
+  }
+
+  // creating a new item (POST) requires a request body
+  // params: 
   function createBookmarks() {
     
   }
 
-  
+
   function deleteBookmarks() {
     
   }
@@ -25,118 +83,10 @@ const api = (function() {
 
   // shouldnt need updateBookmark() due to no edit functionality
   return {
-    getItems,
-    createItem,
-    deleteItem,
-  }
+    getBookmarks,
+    createBookmark,
+    deleteBookmark,
+  };
 
 
 })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const api = (function(){
-  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/rich';
-
-  /**
-   * listApiFetch - Wrapper function for native `fetch` to standardize error handling. 
-   * @param {string} url 
-   * @param {object} options 
-   * @returns {Promise} - resolve on all 2xx responses with JSON body
-   *                    - reject on non-2xx and non-JSON response with 
-   *                      Object { code: Number, message: String }
-   */
-  const listApiFetch = function(...args) {
-    // setup var in scope outside of promise chain
-    let error;
-    return fetch(...args)
-      .then(res => {
-        if (!res.ok) {
-          // if response is not 2xx, start building error object
-          error = { code: res.status };
-
-          // if response is not JSON type, place Status Text in error object and
-          // immediately reject promise
-          if (!res.headers.get('content-type').includes('json')) {
-            error.message = res.statusText;
-            return Promise.reject(error);
-          }
-        }
-
-        // otherwise, return parsed JSON
-        return res.json();
-      })
-      .then(data => {
-        // if error exists, place the JSON message into the error object and 
-        // reject the Promise with your error object so it lands in the next 
-        // catch.  IMPORTANT: Check how the API sends errors -- not all APIs
-        // will respond with a JSON object containing message key
-        if (error) {
-          error.message = data.message;
-          return Promise.reject(error);
-        }
-
-        // otherwise, return the json as normal resolved Promise
-        return data;
-      });
-  };
-
-  const getItems = function() {
-    return listApiFetch(BASE_URL + '/items');
-  };
-
-  const createItem = function(name) {
-    const newItem = JSON.stringify({ name });
-    return listApiFetch(BASE_URL + '/items', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: newItem
-    });
-  };
-
-  const updateItem = function(id, updateData) {
-    const newData = JSON.stringify(updateData);
-    return listApiFetch(BASE_URL + '/items/' + id, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: newData
-    });
-  };
-
-  const deleteItem = function(id) {
-    return listApiFetch(BASE_URL + '/items/' + id, {
-      method: 'DELETE'
-    });
-  };
-
-  return {
-    getItems,
-    createItem,
-    updateItem,
-    deleteItem,
-  };
-}());
