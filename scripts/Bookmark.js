bd7133a929e42c1ce7b95b22ef7e5c644aee893e
Robert Wiggins <wigginsro11@gmail.com>
2019-05-09 17:06:01 -0400

    added Bookmark module functionality

--- scripts/Bookmark.js
+++ scripts/Bookmark.js
@@ -2,32 +2,48 @@
 
 'use strict';
 
-/* individual bookmark module */
-(function Bookmark() {
+// individual bookmark module 
+// only allows access to create bookmark and validate bookmark */
+const Bookmark = (function Bookmark() {
 
-  // creates and returns a bookmark with the matching ID
-  createBookmark( id, title, rating, link, description, isDisplayDetailed ) {
-    this.id = id;
-    this.title = id;
-    this.rating = id;
-    this.description = id;
-    this.isDisplayDetailed = id;
-    this.id = id;
+  // creates and returns a bookmark
+  // VALIDATEBOOKMARKLEGALITY NEEDS TO BE CALLED FIRST
+  // uncertain about field data privacy... best way for access?, they are not supposed to be updated except for isDisplayDetailed
+  function createBookmark( title, rating, link, description ) {
+    
+    // SAFETY FORMULA TO ACCESS FIELD DATA LATER FOR DISPLAY?
+    
+    return {
+      id : cuid(),
+      title,
+      rating,
+      link,
+      description,
+      isDisplayDetailed : false,
+    };
   }
 
-  id
-  DataTransferItemListrating
-  link
-  RTCSessionDescription
+  // returns true if input conditions are met, otherwise false
+  function validateBookmarkLegality(title, rating, link, description) {
 
+    // assure all inputs are not empty
+    if (!rating && rating !== 0 && !link && !description) {
+      return false;
+    }
 
+    // INCLUDE LINK CHECK FOR HTTP AND HTTPS
 
+    
+    // fields are valid and bookmark can be created and added
+    return true;
+
+  }
 
 
   return {
-    getId,
-    
-  }
+    validateBookmarkLegality,
+    createBookmark,
+  };
 
 
 })();
